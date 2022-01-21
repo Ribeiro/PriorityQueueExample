6aafa9f5103c2fac335abd056c3acd6322d03561
Geovanny Ribeiro <geovanny.ribeiro@gmail.com>
2022-01-21 17:40:15 -0300

    Initial commit

---
+++ PriorityQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿
+PriorityQueue<string, string> bankQueue = new PriorityQueue<string, string>(new TitleComparer());
+bankQueue.Enqueue("Robert Plant", "Sir");
+bankQueue.Enqueue("Keith Richards", "Mr");
+bankQueue.Enqueue("Tom Jones", "Mr");
+bankQueue.Enqueue("Paul McCartney", "Sir");
+
+Console.WriteLine("Clearing Customers Now");
+while (bankQueue.TryDequeue(out string item, out string priority))
+{
+    Console.WriteLine($"Popped Item : {item}. Priority Was : {priority}");
+}
---
+++ PriorityQueue/TitleComparer.cs
@@ -0,0 +1,14 @@
+﻿
+public class TitleComparer : IComparer<string>
+{
+    private const string SirTitle = "sir";
+
+    public int Compare(string titleA, string titleB)
+    {
+        var titleAIsFancy = titleA.Equals(SirTitle, StringComparison.InvariantCultureIgnoreCase);
+        var titleBIsFancy = titleB.Equals(SirTitle, StringComparison.InvariantCultureIgnoreCase);
+
+        return (titleAIsFancy == titleBIsFancy) ? 0 : titleAIsFancy ? -1 : 1;
+    }
+
+}
